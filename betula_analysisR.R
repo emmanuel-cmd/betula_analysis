e1c9921f0e7041d8d50079cfa5b836c3e0a225d1
Emmanuel Adeleke <emmanuel.adeleke@climateanalytics.org>
2022-11-12 18:37:58 +0100

    continue_office

--- betula_analysisR.R
+++ betula_analysisR.R
@@ -44,42 +44,69 @@ plot(betlocspaug, add =T)
 
 # Interpolate variables over the extent of Augsburg
 
-# Create an empty grid where n is the total number of cells
-grd              <- as.data.frame(spsample(betlocsp, "regular", n=50000))
-names(grd)       <- c("X", "Y")
-coordinates(grd) <- c("X", "Y")
-gridded(grd)     <- TRUE  # Create SpatialPixel object
-fullgrid(grd)    <- TRUE  # Create SpatialGrid object
+# # IDW - Inverse distance weigthing
+# # Create an empty grid where n is the total number of cells
+# grd              <- as.data.frame(spsample(betlocspaug, "regular", n=50000))
+# names(grd)       <- c("X", "Y")
+# coordinates(grd) <- c("X", "Y")
+# gridded(grd)     <- TRUE  # Create SpatialPixel object
+# fullgrid(grd)    <- TRUE  # Create SpatialGrid object
+# 
+# # Add P's projection information to the empty grid
+# #proj4string(P) <- proj4string(P) # Temp fix until new proj env is adopted
+# proj4string(grd) <- proj4string(betlocspaug)
+# 
+# # Interpolate the grid cells using a power value of 2 (idp=2.0)
+# P.idw <- gstat::idw(NO2 ~ 1, betlocspaug, newdata=grd, idp=2.0)
+# 
+# # Convert to raster object then clip to Texas
+# r       <- raster(P.idw)
+# r.m     <- crop(r, augsburg)
+# r.m <- mask(r.m, augsburg)
+# plot(r.m)
+# 
+# plot(augsburg, add =T)
+# # Plot
+# tm_shape(r.m) +
+#   tm_raster(n=10,palette = "RdBu", auto.palette.mapping = FALSE,
+#             title="Predicted precipitation \n(in inches)") +
+#   tm_shape(P) + tm_dots(size=0.2) +
+#   tm_legend(legend.outside=TRUE)
+# 
+# # set up an 'empty' raster, here via an extent object derived from your data
+# e <- extent(augsburg)
+# # e <- e + 1000 # add this as all y's are the same
+# 
+# r <- raster(e, nrow = 100, ncol=100)
+# proj4string(r) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
+# # or r <- raster(xmn=, xmx=,  ...
+# 
+# # you need to provide a function 'fun' for when there are multiple points per cell
 
-# Add P's projection information to the empty grid
-#proj4string(P) <- proj4string(P) # Temp fix until new proj env is adopted
-proj4string(grd) <- proj4string(betlocspaug)
+# Create a grid
+bbox <- st_bbox(betlocspaug)
+bbox
 
-# Interpolate the grid cells using a power value of 2 (idp=2.0)
-P.idw <- gstat::idw(NO2 ~ 1, betlocspaug, newdata=grd, idp=2.0)
+cell_size <- 5
 
-# Convert to raster object then clip to Texas
-r       <- raster(P.idw)
-r.m     <- crop(r, augsburg) 
-r.m <- mask(r.m, augsburg)
-plot(r.m)
+x <- seq(as.vector(bbox$xmin), as.vector(bbox$xmax), by=cell_size)
+y <- seq(bbox$ymin, bbox$ymax, by=cell_size)
 
-plot(augsburg, add =T)
-# Plot
-tm_shape(r.m) + 
-  tm_raster(n=10,palette = "RdBu", auto.palette.mapping = FALSE,
-            title="Predicted precipitation \n(in inches)") + 
-  tm_shape(P) + tm_dots(size=0.2) +
-  tm_legend(legend.outside=TRUE)
+meuse_grid <- expand.grid(x=x, y=y)
+plot(meuse_grid$x, meuse_grid$y, pch=19, cex=0.1)
 
-# set up an 'empty' raster, here via an extent object derived from your data
-e <- extent(augsburg)
-# e <- e + 1000 # add this as all y's are the same
+meuse_grid <- expand.grid(x=x, y=y)
+plot(meuse_grid$x, meuse_grid$y, pch=1)
 
-r <- raster(e, nrow = 100, ncol=100)
-proj4string(r) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-# or r <- raster(xmn=, xmx=,  ...
+# KRIGING
 
-# you need to provide a function 'fun' for when there are multiple points per cell
+# create sample variogram
+betloc.v <- gstat::variogram(NO2 ~ 1, betlocspaug)
 
+# fit variogram model
+betloc.vfit <- gstat::fit.variogram(betloc.v, vgm(1, "Sph", 300, 1))
 
+# ordinary kriging
+lz.ok <- gstat::krige(NO2 ~ 1, betlocspaug, grd, betloc.vfit)
+
+# https://doi.org/10.15468/dl.s5wure 
